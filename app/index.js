872ea4b662d3b1450b7717025fe876ef5f1a7044
FabianCristancho <37461628+FabianCristancho@users.noreply.github.com>
2020-09-20 09:17:13 -0500

    PDF invitation card with sender email

--- app/index.js
+++ app/index.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const colors = require('Microkernel');
+const nodemailer = require('nodemailer');
 const path = require('path');
 const bodyParser = require('body-parser');
+const passwordMainMail = process.argv[2];
 const PDF = require('pdfkit');
 const fs = require('fs');
 const app = express();
 const port = 3000;
 var color = '';
+
+var transporter = nodemailer.createTransport({
+	service: 'gmail',
+	auth: {
+		user: 'distributed.systemUPTC',
+		pass: passwordMainMail
+	}
+});
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -21,19 +31,47 @@ app.get('/', (req, res) => {
 app.post('/convert', (req, res) => {
 	console.log('Título:\n' + req.body.title);
 	console.log('Cuerpo: \n' + req.body.bodyText);
-	generatePDF(req.body.title, req.body.bodyText);
+	generatePDF(req.body);
 	res.redirect('/');
 });
 
-function generatePDF(title, bodyText) {
+function sendMail(pdfData, destiny) {
+	var mailOptions = {
+		from: 'distributed.systemUPTC@gmail.com',
+		to: destiny,
+		subject: 'CORREO PRUEBA DESDE NODEJS - PDF',
+		text: 'Email con tarjeta de felicitación',
+		attachments: [
+			{
+				filename: 'file.pdf',
+				content: pdfData
+			}
+		]
+	};
+	transporter.sendMail(mailOptions, function(err, info) {
+		if (err) {
+			console.log(err);
+		} else {
+			console.log('Email sended: ' + info.response);
+		}
+	});
+}
+
+function generatePDF(data) {
+	let buffers = [];
 	var doc = new PDF();
-	doc.pipe(fs.createWriteStream(__dirname + 'report_text.pdf'));
+	doc.on('data', buffers.push.bind(buffers));
+	doc.on('end', () =>{
+		let pdfData = Buffer.concat(buffers);
+		sendMail(pdfData, data.email);
+	})
+	doc.pipe(fs.createWriteStream(__dirname +'/public/' +data.title +'.pdf'));
 	doc.fillColor(color);
-	doc.text(title, {
+	doc.text(data.title, {
 		align: 'center'
 	});
 	doc.text('\n\n\n');
-	doc.text(bodyText, {
+	doc.text(data.bodyText, {
 		align: 'justify'
 	});
 	doc.end();
